34d54b30fc940529e23d20bba80f4fae88146b8a
y00406703 <yangzijiang2@huawei.com>
2018-04-02 17:30:11 +0800

    删掉不再用到的代码

--- iosTest2017/iosTest2017/AppDelegate.m
+++ iosTest2017/iosTest2017/AppDelegate.m
@@ -17,17 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [self compare:@"1.0" num2:@"1.0.1"];
-    [self compare:@"1.0" num2:@"1."];
-    [self compare:@"1.0" num2:@"1"];
-    [self compare:@"1.0" num2:@"0.11"];
     return YES;
-}
-
-
-- (void)compare:(NSString *)num1 num2:(NSString*)num2{
-    BOOL result = ([num1 compare:num2 options:NSNumericSearch] == NSOrderedDescending) && ![num1 isEqualToString:num2];
-    NSLog(@"num1 = %@, num2 = %@, result = %d", num1, num2, result);
 }
 
 @end
