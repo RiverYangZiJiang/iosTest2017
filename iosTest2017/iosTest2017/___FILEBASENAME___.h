b03026f3bbd3d8f3398222c33bfd34952347a636
RiverYang <riveryzj@163.com>
2019-02-17 18:24:50 +0800

    增加单元格模板文件

---
+++ iosTest2017/iosTest2017/___FILEBASENAME___.h
@@ -0,0 +1,14 @@
+////___FILEHEADER___
+//
+//___IMPORTHEADER_cocoaTouchSubclass___
+//
+//NS_ASSUME_NONNULL_BEGIN
+//
+//@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
+//
+//+ (___FILEBASENAME___ *)cellWithTableView:(UITableView *)tableView;
+//
+//+ (CGFloat)heightForModel:(<#ModelType#> *)model;
+//@end
+//
+//NS_ASSUME_NONNULL_END
---
+++ iosTest2017/iosTest2017/___FILEBASENAME___.m
@@ -0,0 +1,40 @@
+////___FILEHEADER___
+//
+//#import "___FILEBASENAME___.h"
+//#import "UILabel+MLCategory.h"
+//
+//@interface ___FILEBASENAMEASIDENTIFIER___ ()
+//
+//@end
+//
+//@implementation ___FILEBASENAMEASIDENTIFIER___
+//
+//#pragma mark - LifeCycle
+//+ (___FILEBASENAME___ *)cellWithTableView:(UITableView *)tableView{
+//    static NSString *ID = @"___FILEBASENAME___";
+//    ___FILEBASENAMEASIDENTIFIER___ *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+//    if (!cell) {
+//        cell = [[___FILEBASENAMEASIDENTIFIER___ alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+//    }
+//    return cell;
+//}
+//
+//- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+//    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+//    if (self) {
+//        
+//    }
+//    return self;
+//}
+//
+//#pragma mark - Public
+//+ (CGFloat)heightForModel:(<#ModelType#> *)model{
+//    CGFloat descLabelW = ScreenWidth - 20 - 13;
+//    CGFloat descLabelH = [model.desc heightForFont:font_size_caption width:descLabelW];
+//    
+//    return 45 + descLabelH + 20;
+//}
+//
+//#pragma mark - Getters & Setters
+//
+//@end
